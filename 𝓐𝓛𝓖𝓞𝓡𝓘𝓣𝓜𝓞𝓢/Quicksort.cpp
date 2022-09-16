b10cf892526bdf3ee94bf9ed7c9801f30d953402
Maicken <105568952+Maicken052@users.noreply.github.com>
2022-09-16 08:05:00 -0500

    Update Quicksort.cpp

--- 𝓐𝓛𝓖𝓞𝓡𝓘𝓣𝓜𝓞𝓢/Quicksort.cpp
+++ 𝓐𝓛𝓖𝓞𝓡𝓘𝓣𝓜𝓞𝓢/Quicksort.cpp
@@ -2,35 +2,34 @@
 
 using namespace std;
 
+//-----------------------------------FUNCIÓN PARTICIÓN----------------------------------//
 int particion(int* v, int ini, int fin){
-    int pivote=v[fin];
-    int i=ini-1;
-    int j=ini;
+    int pivote=v[fin]; //Ultimo elemento del vector con el que se comparan los demás elementos
+    int i=ini-1;  //Marcador qe aumenta cuando encuentra un elemento menor que el pivote
+    int j=ini;  //Marcador que recorre el vector
     for(j; j<fin+1; j++){
-        if(v[j]<=pivote){
+        if(v[j]<=pivote){  //Si encuentra un elemento menor que el pivote
             i++;
             int aux=v[i];
             v[i]=v[j];
-            v[j]=aux;
+            v[j]=aux;  //hace el intercambio del elemento del indice i con el elemento que este en el indice j
         }
     }
     return i;
 }
 
+//---------------------------------------QUICKSORT--------------------------------------//
 void quicksort(int* v, int ini, int fin){
-    if(ini==fin){
-         cout<<""<<endl;
+    if(ini>=fin){
+         cout<<"";
     }else{
-        int i=particion(v, ini, fin);
-        if(i-1>ini){
-            quicksort(v, ini, i-1);
-        }
-        if(i+1<fin){
-            quicksort(v, i+1, fin);  
-        }
+        int i=particion(v, ini, fin); 
+        quicksort(v, ini, i-1);
+        quicksort(v, i+1, fin);  //Hace la partición y despues organiza lo que queda en ambos lados
     }   
 }
 
+//-----------------------------------FUNCIÓN PRINT----------------------------------//
 void print(int* v, int size){
     for(int i=0; i<size; i++){
         cout<<v[i]<<"\t";
@@ -39,20 +38,15 @@ void print(int* v, int size){
 }
 
 int main(){
-    int v[10];
-    v[0]= 23;
-    v[1]= 33;
-    v[2]= 584;
-    v[3]= 9300;
-    v[4]= 1;
-    v[5]= 2;
-    v[6]= 3;
-    v[7]= 454;
-    v[8]= -314;
-    v[9]= -314;
+    int v[10]={8, 3, 5, 1, 4, 9, 10, 2, 6, 7};
     
+    cout<<"---------------------------------------------------------------------------"<<endl;
+    cout<<"ORIGINAL: "<<endl;
     print(v, 10);
+    cout<<"---------------------------------------------------------------------------"<<endl;
+    cout<<"ORDENADO: "<<endl;
     quicksort(v, 0, 9);
     print(v, 10);
+    cout<<"---------------------------------------------------------------------------"<<endl;
     return 0;
 }
