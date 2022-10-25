52132b46455a2a0d1194944dde0b589d695644a2
Maicken <105568952+Maicken052@users.noreply.github.com>
2022-10-25 10:48:18 -0500

    Update set.cpp

--- 𝓐𝓛𝓖𝓞𝓡𝓘𝓣𝓜𝓞𝓢/Estructuras de datos/set.cpp
+++ 𝓐𝓛𝓖𝓞𝓡𝓘𝓣𝓜𝓞𝓢/Estructuras de datos/set.cpp
@@ -16,6 +16,10 @@ public:
     }
     
 //------------------------------------------------METODOS-----------------------------------------//
+    int getSize(){
+        return size;
+    }
+    
     bool contains(int x){
         for(int i = 0; i<size; i++){
             if (x == v[i]){
@@ -78,21 +82,54 @@ public:
 
     
     set unionn(set c){
-        set u = c;
+        int size_u = size+c.getSize();
+        set u = set(size_u);
         for(int i = 0; i<size; i++){
-            u.push(v[i]);    
+            u.push(v[i]);
+        }
+        for(int i = 0; i<c.getSize(); i++){
+            u.push(c.v[i]);
         }
         return u; 
     }
     
     set interseccion(set c){
-        set n = new set();
-        for(int i = 0; i<size; i++){
-            if(c.contains(v[i])){
-               n. 
+        int n_cap;
+        n_cap = min(size, c.getSize());
+        set n = set(n_cap);
+        if(n_cap == size){
+            for(int i = 0; i<n_cap; i++){
+                if(c.contains(v[i])){
+                    n.push(v[i]);
+                }    
+            }
+        }else{
+            for(int i = 0; i<n_cap; i++){
+                if(contains(c.v[i])){
+                    n.push(c.v[i]);
+                }    
             }
         }
+        return n;
     }
+    
+    set A_B(set c){
+        int A_B_cap;
+        set I = interseccion(c);
+        A_B_cap = size-I.getSize();
+        set A_B = set(A_B_cap);
+        for(int i = 0; i<size; i++){
+            if(i<I.getSize()){
+                if(not I.contains(v[i])){
+                    A_B.push(v[i]);
+                }
+            }else{
+                A_B.push(v[i]);
+            }
+        }
+        return A_B;
+    }
+    
     void print() {
         for (int i = 0; i <size; i++) {
             cout << v[i] << "\t";
@@ -115,16 +152,23 @@ int main(){
     prueba.push(6);
     prueba.push(5);
     prueba.push(4);
-    prueba.push(4);
+    un.push(4);
     un.push(9);
     un.push(8);
     un.push(7);
     cout<<"-----------------------------------------"<<endl;
-    cout<<"La union de el conjunto 1: "<<endl;
+    cout<<"La union de el conjunto A: "<<endl;
     prueba.print();
-    cout<<"y el conjunto 2: "<<endl;
+    cout<<"y el conjunto B: "<<endl;
     un.print();
     cout<<"es: "<<endl;
     prueba.unionn(un).print();
     cout<<"------------------------------------------"<<endl;
+    cout<<"Y la interseccion es: "<<endl;
+    prueba.interseccion(un).print();
+     cout<<"------------------------------------------"<<endl;
+    cout<<"Y A-B es: "<<endl;
+    prueba.A_B(un).print();
+    
+    return 0;
 }
