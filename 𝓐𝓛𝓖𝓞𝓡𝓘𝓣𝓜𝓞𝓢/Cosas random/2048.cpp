72e4d7866b53577da3a895e93188e14298b1aa24
Maicken <105568952+Maicken052@users.noreply.github.com>
2022-10-27 18:13:49 -0500

    Update 2048.cpp

--- 𝓐𝓛𝓖𝓞𝓡𝓘𝓣𝓜𝓞𝓢/Cosas random/2048.cpp
+++ 𝓐𝓛𝓖𝓞𝓡𝓘𝓣𝓜𝓞𝓢/Cosas random/2048.cpp
@@ -2,42 +2,47 @@
 #include<iostream>
 using namespace std;
 
+//------------------------------CLASE NODO---------------------------------//
 class Nodo{
-  
-  int dato;
-  Nodo* pointer;
+    //Atributos
+    int dato;
+    Nodo* pointer;
   
 public:
+    //Constructores
+    Nodo(){
+        dato = 0;
+        pointer = NULL;
+    }
   
-  Nodo(){
-      dato = 0;
-      pointer = NULL;
-  }
+    Nodo(int d){
+        dato = d;
+        pointer = NULL;
+    }
   
-  ~Nodo(){
-  }
+    //Destructor
+    ~Nodo(){
+    }
   
-  Nodo(int d){
-      dato = d;
-      pointer = NULL;
-  }
+    //Getters
+    int getDato(){
+        return dato;
+    }
+  
+    Nodo* getNext(){
+        return pointer;
+    }
+  
+    //Setters
+    void setDato(int d){
+        dato = d;
+    }
+  
+    void setNext(Nodo* p){
+        pointer = p;
+    }
     
-  int getDato(){
-      return dato;
-  }
-  
-  void setDato(int d){
-      dato = d;
-  }
-  
-  Nodo* getNext(){
-      return pointer;
-  }
-  
-  void setNext(Nodo* p){
-      pointer = p;
-  }
-    
+    //Sobrecargas
     string to_string() {
 		return std::to_string(dato);
 	}
@@ -49,18 +54,20 @@ public:
     
 };
 
+//------------------------------CLASE STACK---------------------------------//
 class Stack{
-    
+    //Atributos
     Nodo* ptr;
     int size;
 
 public:  
-
+    //Constructor
     Stack(){
         ptr = NULL;
         size = 0;
     }
     
+    //Destructor
     ~Stack(){
         Nodo* t = ptr;
         Nodo* n;
@@ -72,6 +79,7 @@ public:
         delete t;
     }
     
+    //Getters
     int getSize(){
         return size;
     }
@@ -80,6 +88,7 @@ public:
         return ptr;
     }
     
+    //Metodos
     void push(int d){
         Nodo* front = ptr;
         Nodo* top = new Nodo(d);
@@ -88,39 +97,24 @@ public:
         size++;
     }
    
-    int pop(){
-        int n2 = ptr->getDato();
-        Nodo* n = ptr;
-        ptr = n->getNext();
-        delete n;
-        size--;
-        return n2;
+    void pop(){
+        if(size == 0){
+            cout<<"El stack esta vacio"<<endl;
+        }else{
+            Nodo* n = ptr;
+            ptr = n->getNext();
+            delete n;
+            size--;
+        }
     }
     
     int peek(){
         return ptr->getDato();
     }
-       
-    Nodo* get(int i){
-        if(i < size && i>=0){
-            Nodo* n = ptr;
-            for(int x = 0; x<i;x++){
-                n = n->getNext();
-            }
-            return n;
-        }else{
-            if(size == 0){
-                cout<<"La lista está vacía";
-            }else{
-                cout<<"La posicion no existe";
-            }
-            return NULL;
-        }
-    }
     
     void print(){
         if(size == 0){
-            cout<<"La lista está vacía"<<endl;
+            cout<<"El stack esta vacio"<<endl;
         }else{
             Nodo* t = ptr;
             do{
@@ -132,9 +126,24 @@ public:
     }
 };
 
+//------------------------------FUNCION POTENCIA DE DOS---------------------------------//
+int es_potencia_dos(int x){
+    while(x%2 == 0){
+        x = x/2;
+    }
+    return x;
+}
 
-string jugar(Stack x){
+//------------------------------FUNCION 2048---------------------------------//
+void jugar(){
+    Stack x = Stack();
+    bool fin = false;
+    
     do{
+        /************************************************************************************
+        Revisa que el número ingresado sea valido, de lo contrario pide de nuevo el dato al
+        usuario.
+        ************************************************************************************/
         string r, r_str;
         bool f = true;
         int r_int;
@@ -142,26 +151,39 @@ string jugar(Stack x){
         while(f){
             try{
                 f = false;
-                cout<<"Ingrese una potencia de dos: "<<endl;
+                cout<<"𝗜𝗻𝗴𝗿𝗲𝘀𝗲 𝘂𝗻𝗮 𝗽𝗼𝘁𝗲𝗻𝗰𝗶𝗮 𝗱𝗲 𝗱𝗼𝘀 𝗺𝗲𝗻𝗼𝗿 𝗾𝘂𝗲 𝟭𝟮𝟴: "<<endl;
                 getline(cin, r);
                 r_int = stoi(r);
                 string r_str = to_string(r_int);
                 if(r != r_str){
                     throw "error";
+                }else if(r_int > 64 || r_int == 1 || r_int == 0){
+                    throw "error";    
+                }else if(es_potencia_dos(r_int) != 1){
+                    throw "error";    
                 }
+                
             }catch(...){
-                cout<<"𝐈𝐧𝐠𝐫𝐞𝐬𝐞 𝐮𝐧 𝐧𝐮𝐦𝐞𝐫𝐨 𝐯𝐚𝐥𝐢𝐝𝐨"<<endl;
+                cout<<"---------------------------------"<<endl;
+                cout<<"𝗜𝗻𝗴𝗿𝗲𝘀𝗲 𝘂𝗻 𝗻𝘂𝗺𝗲𝗿𝗼 𝘃𝗮𝗹𝗶𝗱𝗼"<<endl;
+                cout<<"---------------------------------"<<endl;
                 f = true;
             }
         }
+        
         x.push(r_int);
         
+        /************************************************************************************
+        Si el número agregado es igual al que estaba en el tope, se suman. Si el nuevo numero
+        resultante es igual a de abajo suyo, ocurre lo mismo hasta que el número de abajo sea
+        diferente.
+        ************************************************************************************/
         if(x.getSize() > 1){
             while(x.getPtr()->getNext()->getDato() == x.peek()){
-                int nn = x.getPtr()->getNext()->getDato() + x.peek();
+                int equal = x.peek()*2;
                 x.pop();
                 x.pop();
-                x.push(nn);
+                x.push(equal);
                 
                 if(x.getSize() == 1){
                     break;
@@ -169,22 +191,28 @@ string jugar(Stack x){
             }
         }
         
-        cout<<"Tu juego es: "<<endl;
+        cout<<"---------------------------------"<<endl;
+        cout<<"𝗧𝘂 𝗷𝘂𝗲𝗴𝗼 𝗲𝘀: "<<endl;
         x.print();
+        cout<<"---------------------------------"<<endl;
     
         if(x.peek() == 128){
-            x.pop();
-            cout<<"Lograste un 128!, tu juego es: "<<endl;
-            x.print();
-        }    
-    }while(x.getSize() != 0);
-    
-    return "Felicidades profe :)";
+            if(x.getSize() == 1){
+                cout<<"𝗚𝗔𝗡𝗔𝗦𝗧𝗘𝗘𝗘 🥳"<<endl;
+                fin = true;
+            }else{
+                x.pop();
+                cout<<"𝗟𝗼𝗴𝗿𝗮𝘀𝘁𝗲 𝘂𝗻 𝟭𝟮𝟴!, 𝘁𝘂 𝗷𝘂𝗲𝗴𝗼 𝗻𝘂𝗲𝘃𝗼 𝗲𝘀: "<<endl;
+                x.print();
+            }
+            cout<<"---------------------------------"<<endl;
+        }
+        
+    }while(!fin);
 }    
 
 
 int main(){
-    Stack dmco = Stack();
-    cout<<jugar(dmco)<<endl;
+    jugar();
     return 0;
 }
