a40a4cb62d130c160326eb0b110ecfda399bca44
Maicken <105568952+Maicken052@users.noreply.github.com>
2022-11-16 08:55:06 -0500

    Update Grafo.cpp

--- 𝓐𝓛𝓖𝓞𝓡𝓘𝓣𝓜𝓞𝓢/Estructuras de datos/Grafo.cpp
+++ 𝓐𝓛𝓖𝓞𝓡𝓘𝓣𝓜𝓞𝓢/Estructuras de datos/Grafo.cpp
@@ -4,25 +4,23 @@ using namespace std;
 
 class Grafo{
     int **matriz;
-    int nFilas;
-    int nCol;
+    int nodos;
+    
 public:
-    Grafo(int nFilass, int nColl){
-        nFilas = nFilass;
-        nCol = nColl;
-        matriz= new int*[nFilas];          
-        for(int i=0; i<nFilas; i++){
-            matriz[i]=new int[nCol];
+    Grafo(int Nodos_){
+        nodos = Nodos_;
+        matriz = new int*[nodos];          
+        for(int i = 0; i < nodos; i++){
+            matriz[i] = new int[nodos];
         }
             
-        for(int i=0;i<nFilas;i++){
-            for(int j=0;j<nCol;j++){
-                *(*(matriz+i)+j) = 0;
+        for(int i = 0;i < nodos;i++){
+            for(int j = 0;j < nodos;j++){
+                matriz[i][j] = 0;
             }
         }
     }
         
-    //Getters
     int getPeso(int n1, int n2){
         return matriz[n1][n2];
     }
@@ -39,18 +37,16 @@ public:
     
     void addNodo(){
         int **n_matriz;
-        int old_nFilas = nFilas;
-        int old_nCol = nCol;
-        nFilas +=1;
-        nCol +=1;
-        n_matriz= new int*[nFilas];          
-            for(int i=0; i<nFilas; i++){
-                n_matriz[i]=new int[nCol];
+        int old_nodos = nodos;
+        nodos +=1;
+        n_matriz = new int*[nodos];          
+            for(int i = 0; i < nodos; i++){
+                n_matriz[i] = new int[nodos];
             }
             
-        for(int i=0;i<nFilas;i++){
-            for(int j=0;j<nCol;j++){
-                if(i>=old_nFilas || j>=old_nCol){
+        for(int i = 0;i < nodos;i++){
+            for(int j = 0;j < nodos;j++){
+                if(i >= old_nodos || j >= old_nodos){
                     *(*(n_matriz+i)+j) = 0;
                 }else{
                 *(*(n_matriz+i)+j) = *(*(matriz+i)+j);
@@ -58,33 +54,56 @@ public:
             }
         }
         
-        for(int i=0;i<old_nFilas;i++){  
+        for(int i = 0;i < old_nodos;i++){  
             delete[] matriz[i];
         }
         
         matriz = n_matriz;
     }
     
+    bool es_bidireccional(){
+        bool flag = true;
+        for(int i = 0;i < nodos;i++){
+            for(int j = 0;j < nodos;j++){
+                if(matriz[i][j] != matriz[j][i]){
+                    flag = false;
+                    return flag;
+                }
+            }
+        }
+        return flag;
+    }
+    
+    bool is_k_saltos(int a, int b, int k){
+        
+    }
+    
     void print(){
         cout<<"La matriz que representa el grafo es: "<<endl<<endl;
         cout<<"        ";
-        for(int m = 1; m < nFilas+1; m++){
-            if(m>9){
+        
+        for(int m = 1; m < nodos+1; m++){  //Numero de las columnas
+            if(m>9)
                 cout<<m<<" |  ";
-            }else{
+            else
                 cout<<m<<"  |  ";
-            }
         }
-        string stuff(nCol*6, '_');
-        cout<<endl<<"       ";
+        
+        string stuff(nodos*6, '=');
+        cout<<endl<<"       ";  //Primera separación
         cout<<stuff<<endl;
+        
         cout<<"    1"<<"  |";
-        for(int i=0; i<nFilas; i++){
-            for(int j=0; j<nCol; j++){
-                cout<<matriz[i][j]<<"  |  ";
+        for(int i = 0; i < nodos; i++){
+            for(int j = 0; j < nodos; j++){
+                if(i == j)
+                    cout<<" "<<"  |  ";
+                else
+                    cout<<matriz[i][j]<<"  |  ";
             }
+            
             cout<<endl<<"       "<<stuff<<endl;
-            if(i+2>nFilas){
+            if(i+2>nodos){
                 break;
             }else if(i+2>9){
                 cout<<"   "<<i+2<<"  |";
@@ -97,18 +116,17 @@ public:
 };
 
 int main(){
-    Grafo g = Grafo(8, 8);
-    g.addEnlace(1, 2, 2, "bidireccional");
-    g.addEnlace(3, 2, 2, "bidireccional");
-    g.addEnlace(4, 2, 3, "bidireccional");
-    g.addEnlace(4, 3, 5, "bidireccional");
-    g.addEnlace(5, 2, 3, "bidireccional");
-    g.addEnlace(1, 5, 1, "bidireccional");
-    g.addEnlace(6, 2, 3, "bidireccional");
-    g.addEnlace(6, 5, 1, "bidireccional");
-    g.addEnlace(5, 7, 2, "bidireccional");
-    g.addEnlace(6, 7, 1, "bidireccional");
-    g.addEnlace(8, 5, 4, "bidireccional");
+    Grafo g = Grafo(6);
+    g.addEnlace(1, 3, 2, "unidireccional");
+    g.addEnlace(3, 4, 2, "unidireccional");
+    g.addEnlace(3, 6, 2, "unidireccional");
+    g.addEnlace(4, 6, 2, "unidireccional");
+    g.addEnlace(1, 2, 3, "unidireccional");
+    g.addEnlace(1, 5, 3, "unidireccional");
+    g.addEnlace(6, 2, 3, "unidireccional");
+    g.addEnlace(5, 6, 5, "unidireccional");
+    
     g.print();
+
     return 0;
 }
